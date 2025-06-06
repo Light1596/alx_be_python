c796a83c92582fec43890b3bc15f06ad6175bf5b
Light Situma <127042301+Light1596@users.noreply.github.com>
2025-06-06 13:44:33 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,11 @@
 def perform_operation(num1, num2,operation):
+     """
+    Performs an arithmetic operation.
+    Args:
+        num1 (int/float): The first number.
+        num2 (int/float): The second number.
+        operation (str): The operation to perform ('add', 'subtract', etc.).
+    """
     match operation:
         case 'add':
             return num1 + num2
